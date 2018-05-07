cb9c8a6034f2890ebce331d73edda0c80a1a835d
Ivett Latak <doleance@gmail.com>
2018-05-07 19:29:52 +0200

    (Add)EndOfDrawingTasksYey

---
+++ week-03/day-05 Drawing/src/StarryNight.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class StarryNight {
+    public static void mainDraw(Graphics graphics) {
+        // Draw the night sky:
+        //  - The background should be black
+        //  - The stars can be small squares
+        //  - The stars should have random positions on the canvas
+        //  - The stars should have random color (some shade of grey)
+
+        graphics.fillRect(0, 0, WIDTH, HEIGHT);
+        for (int i = 0; i < 50; i++) {
+            int grayComponent = (int)(Math.random() * 256);
+            graphics.setColor(new Color(grayComponent, grayComponent, grayComponent));
+            graphics.fillRect((int)(Math.random() * WIDTH), (int)(Math.random() * HEIGHT),
+                    5, 5);
+        }
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
