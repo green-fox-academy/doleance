53ab3bb00d6e61cfdf7f0ab666291c6851b1f831
Ivett Latak <doleance@gmail.com>
2018-05-10 15:15:32 +0200

    (Add)YoshDominoesAreFine_ICanGoToInterfaces

--- week-05/day-03 ClassesAndObjects/Dominoes/src/Dominoes.java
+++ week-05/day-03 ClassesAndObjects/Dominoes/src/Dominoes.java
@@ -10,6 +10,20 @@ public class Dominoes {
         // eg: [2, 4], [4, 3], [3, 5] ...
 
         System.out.println(dominoes);
+
+        List<Domino> sortedDominoes = dominoes;
+        for (int i = 1; i < sortedDominoes.size() - 1; i++) {
+            if (sortedDominoes.get(i - 1).getValues()[1] != sortedDominoes.get(i).getValues()[0]) {
+                int j = i + 1;
+                while ((sortedDominoes.get(i - 1).getValues()[1] != sortedDominoes.get(j).getValues()[0]) && (j < sortedDominoes.size() - 2)) {
+                    j++;
+                }
+                switchElements(sortedDominoes, i, j);
+            }
+        }
+
+        System.out.println(sortedDominoes);
+
     }
 
     static List<Domino> initializeDominoes() {
@@ -21,5 +35,13 @@ public class Dominoes {
         dominoes.add(new Domino(2, 4));
         dominoes.add(new Domino(7, 1));
         return dominoes;
+    }
+
+    public static void switchElements(List<Domino> list, int index1, int index2) {
+        Domino tempElement = list.get(index1);
+        list.remove(index1);
+        list.add(index1, list.get(index2 - 1));
+        list.remove(index2);
+        list.add(index2, tempElement);
     }
 }
