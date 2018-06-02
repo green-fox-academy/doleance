f5b41ce971b816b72ba201f407f2b0d20d088249
Ivett Latak <doleance@gmail.com>
2018-06-02 15:58:20 +0200

    (Add)HÁT_EZT_NEM_HISZEM_EL_MENNYIRE_JÓ_MÁR!

--- week-09/day-05 SpringBoot/springstart/src/main/java/com/greenfoxacademy/springstart/Controllers/HelloWebController.java
+++ week-09/day-05 SpringBoot/springstart/src/main/java/com/greenfoxacademy/springstart/Controllers/HelloWebController.java
@@ -1,15 +1,21 @@
 package com.greenfoxacademy.springstart.Controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
 public class HelloWebController {
 
+    static AtomicLong counter = new AtomicLong();
+
     @RequestMapping("/web/greeting")
-    public String greeting(Model model) {
-        model.addAttribute("name", " World");
-        return "greeting";
+    public String greeting(@RequestParam (name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("counter", counter.incrementAndGet());
+        return "web/greeting";
     }
 }
