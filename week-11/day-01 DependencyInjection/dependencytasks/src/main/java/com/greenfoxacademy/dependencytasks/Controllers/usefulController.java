68835287589d74f702a1520b143085c9f63e5eeb
Ivett Latak <doleance@gmail.com>
2018-06-14 21:59:50 +0200

    (Add)UsefulBgColor_IDontKnowWetherItIsWorkingBecauseIAlwayForgetToDisposeThePortsXD

--- week-11/day-01 DependencyInjection/dependencytasks/src/main/java/com/greenfoxacademy/dependencytasks/Controllers/usefulController.java
+++ week-11/day-01 DependencyInjection/dependencytasks/src/main/java/com/greenfoxacademy/dependencytasks/Controllers/usefulController.java
@@ -1,5 +1,7 @@
 package com.greenfoxacademy.dependencytasks.Controllers;
 
+import com.greenfoxacademy.dependencytasks.Service.UtilityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class usefulController {
 
+    @Autowired
+    UtilityService utilityService;
+
     @GetMapping("/useful")
-    public String chooseUsefulUtility (Model model){
+    public String chooseUsefulUtility(){
         return "useful";
+    }
+
+    @GetMapping("/useful/colored")
+    public String coloringBackground (Model model){
+        model.addAttribute("bgcolor", utilityService.randomColor());
+        return "useful/colored";
     }
 }
