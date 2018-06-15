a19ef120017b2807d1e731fa6535a63f83d90309
Ivett Latak <doleance@gmail.com>
2018-06-15 21:47:13 +0200

    (Add)UsefulEmailAndCoding_NowIWillRuinItSoItsASavingSaving

--- week-11/day-01 DependencyInjection/dependencytasks/src/main/java/com/greenfoxacademy/dependencytasks/Controllers/usefulController.java
+++ week-11/day-01 DependencyInjection/dependencytasks/src/main/java/com/greenfoxacademy/dependencytasks/Controllers/usefulController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class usefulController {
@@ -21,5 +23,27 @@ public class usefulController {
     public String coloringBackground (Model model){
         model.addAttribute("bgcolor", utilityService.randomColor());
         return "useful/colored";
+    }
+
+    @GetMapping("/useful/email")
+    public String isValidEmail (@RequestParam(name="email") String email, Model model){
+        model.addAttribute("bgcolor", utilityService.randomColor());
+        model.addAttribute("email", email);
+        model.addAttribute("emailIsOkay", utilityService.validateEmail(email));
+        return "useful/email";
+    }
+
+    @GetMapping("/useful/encode")
+    @ResponseBody
+    public String encoding (@RequestParam(name="text") String text, @RequestParam(name="number") int number, Model model){
+        String fineText = utilityService.caesar(text, number);
+        return fineText;
+    }
+
+    @GetMapping("/useful/decode")
+    @ResponseBody
+    public String decoding (@RequestParam(name="text") String text, @RequestParam(name="number") int number, Model model){
+        String fineText = utilityService.caesar(text, -number);
+        return fineText;
     }
 }
--- week-11/day-01 DependencyInjection/dependencytasks/src/main/java/com/greenfoxacademy/dependencytasks/Service/UtilityService.java
+++ week-11/day-01 DependencyInjection/dependencytasks/src/main/java/com/greenfoxacademy/dependencytasks/Service/UtilityService.java
@@ -23,4 +23,41 @@ public class UtilityService {
     public String randomColor() {
         return colors.get(random.nextInt(colors.size()));
     }
+
+    public boolean validateEmail(String email) {
+        int charPoz = findChar(email,'@');
+        if (charPoz == -1) {
+            return false;
+        } else {
+            String remainedEmail = email.substring(charPoz + 1, email.length());
+            charPoz = findChar(remainedEmail,'.');
+            System.out.println(remainedEmail + charPoz);
+            if ((charPoz != -1) && (charPoz < remainedEmail.length() - 1)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public int findChar(String where, char charToFind) {
+        int i=1;
+        while ((where.charAt(i) != charToFind) && (i < where.length() - 1)) {
+            i++;
+        }
+        if (i == where.length() - 1) {
+            return -1;
+        } else {
+            return i;
+        }
+    }
+
+    public String caesar(String text, int number) {
+        String result = "";
+        for(int i = 0; i < text.length(); i++) {
+            result += (char)((int)text.charAt(i) + number);
+        }
+        return result;
+    }
+
 }
