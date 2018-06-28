3bca0437f48f5d6d71d76fffebe76cb19b444604
Ivett Latak <doleance@gmail.com>
2018-06-28 23:22:53 +0200

    (Add)JustMakingTheBedForTheTTC_NothingMuch

---
+++ week-12 TTC/src/main/java/com/greenfoxacademy/ttc/TtcApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.ttc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtcApplication.class, args);
+    }
+}
